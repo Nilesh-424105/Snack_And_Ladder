c637fdfbbd3f41c7ac0c1a7cf121be4648aa3a58
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-02 15:44:07 +0530

    ADDED: Welcome_Message

---
+++ snack_And_Ladder.java
@@ -0,0 +1,8 @@
+package com.snackAndLadder;
+
+public class snack_And_Ladder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snack And Ladder Computation Program");
+	}
+
+}
